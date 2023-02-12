a12c7f776ff42169e7c758c0c4c86269c94ffbbb
cuisine <cuiguopeng@hotmail.com>
2023-02-12 18:53:30 +0800

    移除项目

--- README.md
+++
@@ -1,3 +0,0 @@
-# zenve
-zenve project
-> START OF HERE
--- src/main/java/com/zenve/chat/repository/MessageGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zenve.chat.repository;
-
-import com.zenve.chat.entity.MessageGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageGroupRepository extends JpaRepository<MessageGroup, Long> {
-}
